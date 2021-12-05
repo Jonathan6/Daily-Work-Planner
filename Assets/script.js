c24d36dcc88f2070e00e2246722a88716e91b88f
Jonathan Chen <jontkc98@gmail.com>
2021-12-05 11:50:33 -0800

    Changing html from custom to bootstrap.

--- Assets/script.js
+++ Assets/script.js
@@ -1,14 +1,26 @@
 var numHours = 24;
+var date = $("#currentDay");
 var container = $(".container");
-var hours = [];
+var currentHour = moment().format("H");
 
-console.log(container);
-for (var i = 0; i < numHours; i++) {
-    hours.push($("<div>"));
-    hours[i].appendTo(container);
-    hours[i].text("Hour" + i);
+var timerInterval = setInterval(function() {
+    currentHour = moment().format("H");
+    console.log(currentHour);
+},60000);
+
+var blockBank = $(".block");
+console.log(blockBank);
+date[0].textContent = moment().format("dddd, MMM Do hh:mm");
+
+for (var i = 0; i < blockBank.length; i++) {
+    if (currentHour > blockBank[i].dataset.num) {
+        blockBank[i].children[1].style.background = "grey";
+    } else if (currentHour == blockBank[i].dataset.num) {
+        blockBank[i].children[1].style.background = "red"
+    } else {
+        blockBank[i].children[1].style.background = "green"
+    }
 }
-
 /*
     make all the hour objects
         input
@@ -22,3 +34,45 @@ for (var i = 0; i < numHours; i++) {
         refresh every min
         checks if hour is after the time
 */
+var authorname = "hello";
+var testEl = $(`<p>${authorname}</p>`);
+
+
+var dataToIndex = 8;
+var inputEls = $(".input");
+var scheduleText = ["","","","","","","","","",""];
+
+container.on("click", function (event) {
+    /*  If the event is on a button then
+        we take the corresponding input value
+        set the input as the default
+    */
+   console.log();
+    if (event.target.localName == "button") {
+        var targetIndex = event.target.dataset.num - dataToIndex;
+        var whatWeWant = inputEls[targetIndex - 8].value;
+        scheduleText[targetIndex] = whatWeWant;
+    }
+//    need the input box
+//      need the text from input box
+//      set it as the default for the input
+//      save it in local storage
+});
+
+function save() {
+    localStorage.setItem("scheduleText", JSON.stringify.scheduleText);
+}
+
+function load() {
+    if (localStorage.getItem("scheduleText") !== null) {
+        scheduleText = JSON.parse(localStorage.getItem("scheduleText"));
+    }
+}
+
+function renderSchedule() {
+
+}
+
+// need to load the page of everything in the local storage
+    // we're just making an array to make thing easier
+// need to clear local storage once the time has passed?
