4470d5c6abc25ca9a6eff4ac926d899db0e227d2
Jonathan Chen <jontkc98@gmail.com>
2021-12-08 17:00:46 -0800

    Added readme screenshot of project. Cleaned up HTML and javascript to be more concise and consistent whitespace

--- Assets/script.js
+++ Assets/script.js
@@ -1,46 +1,49 @@
-// Settings
-var numHours = 24;
+// HTML Elements
+var dateEl = $("#currentDay");
+var containerEl = $(".container");
+var hourEl = $(".block");
 
-var date = $("#currentDay");
-var container = $(".container");
-var blockBank = $(".block");
-
-var currentHour = moment().format("H");
+// Time Variables
+var currentHour = parseInt(moment().format("H"));
 var currentDate = moment().format("YYYY MM DD");
 
+// Array storing plans for the day
 var scheduleText = {};
 
-var timerInterval = setInterval(function() {
-    currentHour = moment().format("H");
-    console.log(currentHour);
-},60000);
+var timerInterval = setInterval(refresh(),60000);
 
 
-// Gathering all the block elements into a single variable to change color depending on the time
-date[0].textContent = moment().format("dddd, MMM Do hh:mm");
-
-for (var i = 0; i < blockBank.length; i++) {
-    if (currentHour > blockBank[i].textContent) {
-        blockBank[i].style.background = "grey";
-    } else if (currentHour == blockBank[i].textContent) {
-        blockBank[i].style.background = "red"
-    } else {
-        blockBank[i].style.background = "green"
-    }
-}
-
-container.on("click", function (event) {
+// Event Listener for save buttons
+containerEl.on("click", function (event) {
     if (event.target.localName === "button") {
-        scheduleText[event.target.dataset.id] = $("#" + event.target.dataset.id).val();
-        
+        scheduleText[event.target.dataset.id] = $("#" + event.target.dataset.id).val().trim();
         save();
     }
 });
 
+// Updates the variable elements on the page every minute
+function refresh() {
+    dateEl[0].textContent = moment().format("dddd, MMM Do hh:mm");
+    
+    for (var i = 0; i < hourEl.length; i++) {
+        var blockHour = parseInt(hourEl[i].dataset.hour);
+
+        if (currentHour > blockHour) {
+            hourEl[i].style.background = "grey";
+        } else if (currentHour == blockHour) {
+            hourEl[i].style.background = "red";
+        } else {
+            hourEl[i].style.background = "green"
+        }
+    }
+}
+
+// Save scheduleText to localStorage
 function save() {
     localStorage.setItem(currentDate, JSON.stringify(scheduleText));
 }
 
+// Loads scheduleText from localStorage
 function load() {
     for (var i = 0; i < localStorage.length; i++) {
         var key = localStorage.key(i);
@@ -51,15 +54,13 @@ function load() {
 
 }
 
+// Renders today's schedule using scheduleText
 function renderSchedule() {
     for (id in scheduleText) {
         $("#" + id).val(scheduleText[id]);
     }
 }
 
+refresh();
 load();
 renderSchedule();
-
-// need to load the page of everything in the local storage
-    // we're just making an array to make thing easier
-// need to clear local storage once the time has passed?
